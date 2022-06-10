57e75bcd6950491887eee7a9c0381c7633ff6bbf
Darius Murawski <darius@murawski.blog>
2022-06-10 22:38:39 +0200

    support only input argument, make fixed order of keys on output

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
-use csv::Reader;
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
+
+
+use csv::{Reader, StringRecord};
 
 pub fn arg_parse(args: Vec<String>) -> Options {
     let input: String = String::from("--input");
@@ -10,6 +12,13 @@ pub fn arg_parse(args: Vec<String>) -> Options {
     let quiet: String = String::from("--quiet");
 
     let mut options = Options::default();
+    // assume only input provided
+    if args.len() == 2 {
+        let input_csv = args[1_usize].clone();
+        options.input = input_csv.clone();
+        options.output = input_csv.clone().to_string() + ".json";
+        return options;
+    }
     for (i, a) in args.iter().enumerate() {
         if input.eq(a) {
             let input_csv = args[i + 1_usize].clone();
@@ -28,48 +37,51 @@ pub fn arg_parse(args: Vec<String>) -> Options {
     return options;
 }
 
-pub fn build_json_line(record: HashMap<String, String>) -> String {
+pub fn build_json_line(record: HashMap<String, String>, header: StringRecord) -> String {
     let mut line = "{".to_string();
-    for (k, v) in record {
+    // consistent key order
+    for h in &header {
+        let value = (record.get(h).unwrap()).to_string();
         line.push('"');
-        line.push_str(&k.replace("\"", "\\\""));
+        line.push_str(&h.replace("\"", "\\\""));
         line.push_str("\":\"");
-        line.push_str(&v.replace("\"", "\\\""));
+        line.push_str(&value.replace("\"", "\\\""));
         line.push_str("\",");
     }
+
     // remove last comma
     let mut a = line[0..line.len() - 1].to_string();
     a.push_str("}\n");
     a
 }
 
-pub fn read_data(options: &Options) -> Vec<HashMap<String, String>> {
+pub fn read_data(options: &Options) -> (Vec<HashMap<String, String>>, StringRecord) {
     let mut rdr = Reader::from_path(&options.input).unwrap();
-    let headers = rdr.headers().unwrap().clone();
+    let header = rdr.headers().unwrap().clone();
     let data: Vec<HashMap<String, String>> = rdr
         .records()
         .map(|record| {
-            Ok(headers
+            Ok(header
                 .iter()
                 .map(|e| e.to_string())
                 .zip(record?.iter().map(String::from))
                 .collect())
         })
         .collect::<Result<_, Box<dyn Error>>>().unwrap();
-    data
+    (data, header)
 }
 
 pub fn run(options: Options) -> Result<(), Box<dyn Error>> {
-    let data = read_data(&options);
+    let (data, header) = read_data(&options);
     if options.output.is_empty() {
         for record in data {
-            let line = build_json_line(record);
+            let line = build_json_line(record, header.clone());
             print!("{}", line)
         }
     } else {
         let mut file_handler = File::create(&options.output).unwrap();
         for record in data {
-            let line = build_json_line(record);
+            let line = build_json_line(record, header.clone());
             let b = line.as_bytes();
             file_handler.write_all(b).unwrap();
         }
@@ -107,13 +119,16 @@ impl Default for Options {
 #[cfg(test)]
 mod test {
     use std::collections::HashMap;
+    use csv::StringRecord;
 
     #[test]
     fn test_build_json_line() {
         use super::build_json_line;
         let mut data = HashMap::new();
+        let mut header = StringRecord::new();
+        header.push_field("test-key");
         data.insert(String::from("test-key"), String::from("test-value"));
-        let json = build_json_line(data);
+        let json = build_json_line(data, header);
         assert_eq!(json, "{\"test-key\":\"test-value\"}\n")
     }
 
@@ -121,8 +136,10 @@ mod test {
     fn test_buid_json_line_with_doublequotes() {
         use super::build_json_line;
         let mut data = HashMap::new();
+        let mut header = StringRecord::new();
+        header.push_field("test-key");
         data.insert(String::from("test-key"), String::from("test\"-value"));
-        let json = build_json_line(data);
+        let json = build_json_line(data, header);
         assert_eq!(json, "{\"test-key\":\"test\\\"-value\"}\n")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use std::collections::HashMap;
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::Write;
-
-use csv::{Reader, StringRecord};
 use csv_to_json::arg_parse;
 
 /** This converts a csv file to a json file.
