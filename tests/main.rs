82c84020e1a4ab459642893e676a15ef8e8b56fe
Darius Murawski <darius@murawski.blog>
2022-08-09 22:03:47 +0200

    use assert_cmd for end2end tests

--- tests/main.rs
+++ tests/main.rs
@@ -1,66 +1,82 @@
 extern crate core;
 
 use std::fs;
-use csv_to_json::ApplicationOptions;
+use std::path::PathBuf;
+
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn test_show_help() {
+    let mut command = Command::cargo_bin("csv_to_json").unwrap();
+    let assert = command.arg("--help").assert();
+
+    assert.success().stdout(
+        predicate::str::contains("--help")
+            .and(predicate::str::contains("--input")
+    ).and(predicate::str::contains("--output"))
+    );
+}
+
 
 #[test]
 fn test_only_path() {
-    let options = ApplicationOptions { input: vec!["test.csv".to_owned()], output: None };
-
-    let r = csv_to_json::run_by_option(&options);
-    match r {
-        Ok(_t) => assert_eq!(true, true),
-        _ => assert_eq!(false, true)
-    }
+    let mut command = Command::cargo_bin("csv_to_json").unwrap();
+    let assert = command.arg("--input").arg("tests/test.csv").assert();
+    assert.success().code(0);
+    fs::remove_file(PathBuf::from("tests/test.csv.json")).unwrap()
 }
 
 #[test]
 fn test_input_output() {
-    let input = vec!["tests/test.csv".to_string()];
-    let output : Option<String> = Some("tests/test.json".to_string());
+    let mut command = Command::cargo_bin("csv_to_json").unwrap();
+    let assert = command
+        .arg("--input")
+        .arg("tests/test.csv")
+        .arg("--output")
+        .arg("tests/test.json")
+        .assert();
 
-    let options: ApplicationOptions = ApplicationOptions { input, output};
-    let r = csv_to_json::run_by_option(&options);
-    match r {        Ok(_t) => {
-            let content = fs::read_to_string("tests/test.json").unwrap();
-            assert_eq!(content, "{\"header_1\":\"Value_1\",\"header_2\":\"value_2\"}\n");
-            fs::remove_file("tests/test.json").unwrap();
-        }
-        _ => assert_eq!(false, true)
-    }
+    assert.success().code(0);
+
+    let content = fs::read_to_string("tests/test.json").unwrap();
+    assert_eq!(content, "{\"header_1\":\"Value_1\",\"header_2\":\"value_2\"}\n");
+    fs::remove_file("tests/test.json").unwrap();
+
 }
 
 #[test]
 fn test_input_by_globbing() {
-    let input = vec!["tests/*.csv".to_string()];
-    let output = Some("output".to_string());
 
-    let options: ApplicationOptions = ApplicationOptions { input, output};
-    let r = csv_to_json::run_by_option(&options);
-    match r {
-        Ok(_t) => {
-            let content = fs::read_to_string("output/tests/test.csv.json").unwrap();
-            assert_eq!(content, "{\"header_1\":\"Value_1\",\"header_2\":\"value_2\"}\n");
-            fs::remove_dir_all("output").unwrap();
-        }
-        _ => assert_eq!(false, true)
-    }
+    let mut command = Command::cargo_bin("csv_to_json").unwrap();
+    let assert = command
+        .arg("--input")
+        .arg("tests/*.csv")
+        .arg("--output")
+        .arg("output")
+        .assert();
+
+    assert.success().code(0);
+
+    let content = fs::read_to_string("output/tests/test.csv.json").unwrap();
+    assert_eq!(content, "{\"header_1\":\"Value_1\",\"header_2\":\"value_2\"}\n");
+    fs::remove_dir_all("output").unwrap();
+
 }
 
 #[test]
 fn test_input_output_by_globbing() {
-    let input = vec!["tests/*.csv".to_string()];
-    let output = None;
 
-    let options: ApplicationOptions = ApplicationOptions { input, output};
-    let r = csv_to_json::run_by_option(&options);
-    match r {
-        Ok(_t) => {
-            let content = fs::read_to_string("tests/test.csv.json").unwrap();
-            assert_eq!(content, "{\"header_1\":\"Value_1\",\"header_2\":\"value_2\"}\n");
-            fs::remove_file("tests/test.csv.json").unwrap();
-        }
-        _ => assert_eq!(false, true)
-    }
+    let mut command = Command::cargo_bin("csv_to_json").unwrap();
+    let assert = command
+        .arg("--input")
+        .arg("tests/*.csv")
+        .assert();
+
+    assert.success().code(0);
+
+    let content = fs::read_to_string("tests/test.csv.json").unwrap();
+    assert_eq!(content, "{\"header_1\":\"Value_1\",\"header_2\":\"value_2\"}\n");
+    fs::remove_file("tests/test.csv.json").unwrap();
 }
 
